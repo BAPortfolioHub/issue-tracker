9fab1a94b9424b05d1de0e59b157bf5896b8da79
arse0039 <arse0039@umn.edu>
2024-04-10 15:25:56 -0700

    Added components from prior version

---
+++ app/(components)/PriorityDisplay.tsx
@@ -0,0 +1,36 @@
+import { faFire } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+interface PriorityDisplayProp {
+  priority: number;
+}
+
+const PriorityDisplay: React.FC<PriorityDisplayProp> = ({ priority }) => {
+  return (
+    <div className="flex justify-start align-baseline">
+      {priority === 1 && (
+        <>
+          <FontAwesomeIcon icon={faFire} className="text-green-400" />
+          <span className="ml-2 mr-2">Low</span>
+          <FontAwesomeIcon icon={faFire} className="text-green-400" />
+        </>
+      )}
+      {priority === 2 && (
+        <>
+          <FontAwesomeIcon icon={faFire} className="text-orange-400" />
+          <span className="ml-2 mr-2">Medium</span>
+          <FontAwesomeIcon icon={faFire} className="text-orange-400" />
+        </>
+      )}
+      {priority === 3 && (
+        <>
+          <FontAwesomeIcon icon={faFire} className="text-red-700" />
+          <span className="ml-2 mr-2">High</span>
+          <FontAwesomeIcon icon={faFire} className="text-red-700" />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default PriorityDisplay;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import { Issue } from "./lib/definitions";
+import IssuePage from "./(components)/DisplayIssues";
 
 const Home: React.FC = () => {
   const [issues, setIssues] = useState<Issue[]>([]);
@@ -22,15 +23,7 @@ const Home: React.FC = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        {issues.map((issue, k) => (
-          <div key={k} className="mb-4">
-            <p>Project: {issue.project}</p>
-            <p>Title: {issue.title}</p>
-            <p>Description: {issue.description}</p>
-            <p>Priority: {issue.priority}</p>
-            <p>Status: {issue.status}</p>
-          </div>
-        ))}
+        <IssuePage issueData={issues} />
       </div>
     </div>
   );
